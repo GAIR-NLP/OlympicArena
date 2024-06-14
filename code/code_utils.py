f9a146556c9224826aa2b5de44e67bef173573ac
Zhen Huang <97585031+HuangZhen02@users.noreply.github.com>
2024-06-14 11:11:24 +0800

    Update code_utils.py

--- code/code_utils.py
+++ code/code_utils.py
@@ -54,13 +54,15 @@ def convert_to_list_of_dicts(input_output_dict):
     return [{"input": inp, "output": out} for inp, out in zip(input_list, output_list)]
 
 
-
 def code_executor(codes, test_cases):
-    with multiprocessing.Pool() as pool:  # Create the pool before applying sandbox restrictions
-        # code_sandbox_setup()
-        test_cases = convert_to_list_of_dicts(test_cases)
-        code_test_pairs = [(code, test_cases) for code in codes]
-        results = pool.map(execute_code, code_test_pairs)
+    test_cases = convert_to_list_of_dicts(test_cases)
+    code_test_pairs = [(code, test_cases) for code in codes]
+    results = []
+    
+    for pair in code_test_pairs:
+        result = execute_code(pair)
+        results.append(result)
+    
     return results
 
 class TimeoutException(Exception):
